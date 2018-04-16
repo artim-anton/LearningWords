d3fc05d9ce51ffd3d6c7c4162aee0b0660c2d017
artim-anton <artim-anton@yandex.ru>
2018-04-16 16:51:24 +0300

    realization of editVerd

--- app/src/main/java/com/artimanton/learningwords/EditVerd.java
+++ app/src/main/java/com/artimanton/learningwords/EditVerd.java
@@ -2,12 +2,41 @@ package com.artimanton.learningwords;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import io.realm.Realm;
 
 public class EditVerd extends AppCompatActivity {
+    private Realm mRealm;
+    EditText edTxVerb;
+    EditText edTxTranslate;
+    EditText esTxExample;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_verd);
+        mRealm = Realm.getInstance(getApplicationContext());
+        edTxVerb = (EditText) findViewById(R.id.verbEditText);
+        edTxTranslate = (EditText) findViewById(R.id.translateEditText);
+        esTxExample = (EditText) findViewById(R.id.exampleEditText);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mRealm.close();
+    }
+
+
+    public void buttonSaveVerb(View view) {
+        mRealm.beginTransaction();
+        Words words = mRealm.createObject(Words.class);
+        words.setVerb(edTxVerb.getText().toString());
+        words.setTranslate(edTxTranslate.toString());
+        words.setExample(esTxExample.toString());
+        mRealm.commitTransaction();
     }
 }
--- app/src/main/java/com/artimanton/learningwords/MainActivity.java
+++ app/src/main/java/com/artimanton/learningwords/MainActivity.java
@@ -1,13 +1,34 @@
 package com.artimanton.learningwords;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
+    private Realm mRealm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mRealm = Realm.getInstance(getApplicationContext());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mRealm.close();
+    }
+
+    public void buttonAddVerb(View view) {
+        Intent intent = new Intent(this, EditVerd.class);
+        startActivity(intent);
+    }
+
+    public void buttonRemoveVerb(View view) {
+
     }
 }
